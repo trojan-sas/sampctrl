047cf5a2894cebc6731c4b88fbba44728e42db32
Lorenc <zeelorenc@users.noreply.github.com>
2015-08-14 21:10:32 +1000

    renamed variables

--- revctrl.php
+++ revctrl.php
@@ -7,17 +7,17 @@ $jsonData = json_decode(file_get_contents("https://www.revctrl.com/api/projects/
 if ($jsonData) {
 	$latestChangelog = $jsonData['latest_changelog'];
 
-	$changesetString = "{FFFFFF}Here are latest changes for the most recent update " . $latestChangelog['version']  . ", rolled out on " . $latestChangelog['updated_at'] . ".\n\n";
+	$changelogString = "{FFFFFF}Here are latest changes for the most recent update " . $latestChangelog['version']  . ", rolled out on " . $latestChangelog['updated_at'] . ".\n\n";
 
 	foreach ($latestChangelog['revisions'] as $key) {
 		// adds an extra tab to short revision types
 		$extraTab = strlen($key['type']) < 7 ? "\t" : "";
 
 		// appends revision after revision
-		$changesetString .= "{C0C0C0}" . $key['type'] . "{FFFFFF}\t" . $extraTab . html_entity_decode(strip_tags($key['revision'])) . "\n";
+		$changelogString .= "{C0C0C0}" . $key['type'] . "{FFFFFF}\t" . $extraTab . html_entity_decode(strip_tags($key['revision'])) . "\n";
 	}
 
-	$changesetString .= "\nPlease make sure to follow " . $jsonData['codename'] . " on {C0C0C0}www.revctrl.com/" . $jsonData['creator'] . "/" . $jsonData['codename'] . "{FFFFFF} to stay always up to date!";
+	$changelogString .= "\nPlease make sure to follow " . $jsonData['codename'] . " on {C0C0C0}www.revctrl.com/" . $jsonData['creator'] . "/" . $jsonData['codename'] . "{FFFFFF} to stay always up to date!";
 
-	echo $changesetString;
+	echo $changelogString;
 }
